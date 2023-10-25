e8bed80a49a2a9e2a184f3bab52f73879108e0ec
devAlex.ᴱᵀᴴ <88956822+devAlexDotEth@users.noreply.github.com>
2023-10-24 21:46:33 -0400

    Web3 Packages

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Container } from './styles';
+import Home from './compositions/home';
+// import DesignSystem from './compositions/design-system';
+
+function App() {
+  
+  return (
+    <Container>
+      <Home />
+      {/* <DesignSystem /> */}
+    </Container>
+  );
+}
+
+export default App;
